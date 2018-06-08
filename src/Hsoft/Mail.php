d793771a54dbbebca0812751cd5c769b84f83d77
haowei4032 <haowei4032@126.com>
2018-06-09 00:08:35 +0800

    add test files

---
+++ src/Hsoft/Mail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hsoft;
+
+class Mail
+{
+	public function __construct()
+	{
+		echo 1;
+	}
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Hsoft\Mail;
+
+$mail = new Mail();
